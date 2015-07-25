929a8916c2d7aa1ec0374de9e00489dde929bdb2
shwn002 <shwn002@gmail.com>
2015-07-25 12:39:33 -0400

    Added favorite selection to multi pane

--- app/src/main/java/example/nanodegree/movieapp/fragment/MovieDetailsFragment.java
+++ app/src/main/java/example/nanodegree/movieapp/fragment/MovieDetailsFragment.java
@@ -6,22 +6,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import example.nanodegree.movieapp.Const;
 import example.nanodegree.movieapp.Movie;
 import example.nanodegree.movieapp.R;
+import example.nanodegree.movieapp.Utils;
 
 
-public class MovieDetailsFragment extends Fragment {
+public class MovieDetailsFragment extends Fragment implements View.OnClickListener {
     View rootView;
  //   static final String TAG = MovieDetailsFragment.class.getSimpleName();
     String title, realeaseDate, plotSynopsis, imagePosterUrl;
@@ -29,6 +33,7 @@ public class MovieDetailsFragment extends Fragment {
     TextView tvTitle, tvReleaseDate, tvPlotSynopsis, tvUserRating;
     ImageView imageViewPoster;
     Movie movie;
+    ImageButton fav_button;
 
     public MovieDetailsFragment() {
     }
@@ -44,6 +49,12 @@ public class MovieDetailsFragment extends Fragment {
 
 
         return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateFavButton();
     }
 
     private void getMovieDetails() {
@@ -63,6 +74,8 @@ public class MovieDetailsFragment extends Fragment {
         tvPlotSynopsis = (TextView) rootView.findViewById(R.id.movie_details_plot_synopsis);
         tvUserRating = (TextView) rootView.findViewById(R.id.movie_details_rating);
         imageViewPoster = (ImageView) rootView.findViewById(R.id.movie_details_image_poster);
+        fav_button = (ImageButton) rootView.findViewById(R.id.favorite_button);
+        fav_button.setOnClickListener(this);
 
         tvTitle.setText(title);
         tvReleaseDate.setText(getFormattedDate());
@@ -85,4 +98,32 @@ public class MovieDetailsFragment extends Fragment {
         return sdf.format(date);
     }
 
+    private void updateFavButton() {
+
+        List<Movie> favoriteMovies = Utils.getFavoriteMovies(getActivity());
+        boolean isFound = false;
+
+        for (int i = 0; i < favoriteMovies.size(); i++)
+            if (favoriteMovies.get(i).getMovieId() == movie.getMovieId())
+                isFound = true;
+
+        if (isFound)
+            fav_button.setImageResource(android.R.drawable.btn_star_big_on);
+        else fav_button.setImageResource(android.R.drawable.btn_star_big_off);
+    }
+
+    private void toogleFavButton() {
+        Utils.updateFavoriteMovieList(getActivity(), movie);
+        updateFavButton();
+        Toast.makeText(getActivity(), "clicked " + Utils.getFavoriteMovies(getActivity()).size(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.favorite_button:
+                toogleFavButton();
+                break;
+        }
+    }
 }
